9dedd662918841293895ee406f13735bf731b508
ovfisher <ovfisher@gmail.com>
2024-10-13 21:50:37 +0300

    add new const

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pygame
 from pygame.examples.sprite_texture import running
 
 pygame.init()
-
+SCREEN_WIDTH=600
+SCREEN_HEGHT=800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEGHT))
+pygame.display.set_caption('Игра Тир')
+icon = pygame.image.load('tir_icon.jpg')
 runnig = True
 while running:
     running = False
