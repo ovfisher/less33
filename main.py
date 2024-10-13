d28beeb4eddedbf79a71d044adbe15a0f0e15d50
ovfisher <ovfisher@gmail.com>
2024-10-13 23:42:11 +0300

    final

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ pygame.display.set_icon(icon)
 target_image = pygame.image.load('images/target.png')
 target_width = 80
 target_height = 80
-target_x = random.randint(0, SCREEN_WIDTH-target_width)
-target_y = random.randint(0,SCREEN_HEGHT-target_height)
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0,SCREEN_HEGHT - target_height)
 color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 running = True
@@ -22,7 +22,7 @@ while running:
         if event == pygame.QUIT:
             running = False
         if event == pygame.MOUSEBUTTONDOWN:
-            mouse_x,mouse_y = pygame.mouse.get_pos()
+            mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x+target_width and target_y < mouse_y < target_y+target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEGHT - target_height)
