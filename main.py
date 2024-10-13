26b2f5e4af40db9f877d47b32dcac7da693a2381
ovfisher <ovfisher@gmail.com>
2024-10-13 22:29:51 +0300

    add new target

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ SCREEN_WIDTH=600
 SCREEN_HEGHT=800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEGHT))
 pygame.display.set_caption('Игра Тир')
-icon = pygame.image.load('image/tir_icon.jpg')
+icon = pygame.image.load('images/tir_icon.jpg')
 pygame.display.set_icon(icon)
-target_image = pygame.image.load('image/')
-target_width = 50
-target_height = 50
+target_image = pygame.image.load('images/target.png')
+target_width = 80
+target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH-target_width)
 target_y = random.randint(0,SCREEN_HEGHT-target_height)
 color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
