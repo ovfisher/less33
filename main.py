03de775f0f4cc038a8f526e8d9567c904d2afa72
ovfisher <ovfisher@gmail.com>
2024-10-13 23:20:12 +0300

    update cycle

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
 import random
-from pygame.examples.sprite_texture import running
 
 pygame.init()
 SCREEN_WIDTH=600
-SCREEN_HEGHT=800
+SCREEN_HEGHT=600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEGHT))
 pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('images/tir_icon.jpg')
@@ -16,10 +15,19 @@ target_x = random.randint(0, SCREEN_WIDTH-target_width)
 target_y = random.randint(0,SCREEN_HEGHT-target_height)
 color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
-runnig = True
+running = True
 while running:
-    running = False
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+        if event == pygame.MOUSEBUTTONDOWN:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x+target_width and target_y < mouse_y < target_y+target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEGHT - target_height)
 
-print('hi git')
-print('hi git')
+    screen.blit(target_image, (target_x,target_y))
+    pygame.display.update()
+
 pygame.quit()
