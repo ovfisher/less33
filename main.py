2b178c1cb87526ab928e43437e30015d5f774b5e
ovfisher <ovfisher@gmail.com>
2024-10-15 17:13:13 +0300

    target

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ running = True
 while running:
     screen.fill(color)
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
-        if event == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x+target_width and target_y < mouse_y < target_y+target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
